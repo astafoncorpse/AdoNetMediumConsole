c836313bc47dfebdd3461db9616dd9cdc3c3a4a6
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-05-17 07:40:09 +0300

    Fix ConnectionString

--- AdoNetMediumConsole/Configuration/ConnectionString.cs
+++ AdoNetMediumConsole/Configuration/ConnectionString.cs
@@ -3,6 +3,6 @@
     
         public static class ConnectionString
         {
-        public static string MsSqlConnection => @"Data Source=.\SQLEXPRESS;Database=testing;Trusted_Connection=True;";
-}
+        public static string MsSqlConnection => @"Server=.\SQLExpress02;Database=testing;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True;";
+    }
     }
