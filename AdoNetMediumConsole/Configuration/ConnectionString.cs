8a72c0d0706da68a2f0f814522806b04024a1f58
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-05-17 07:09:50 +0300

    Добавьте файлы проекта.

---
+++ AdoNetMediumConsole/Configuration/ConnectionString.cs
@@ -0,0 +1,8 @@
+﻿namespace AdoNetMediumConsole.Configuration
+{
+    
+        public static class ConnectionString
+        {
+        public static string MsSqlConnection => @"Data Source=.\SQLEXPRESS;Database=testing;Trusted_Connection=True;";
+}
+    }
---
+++ AdoNetMediumConsole/Configuration/MainConnector.cs
@@ -0,0 +1,51 @@
+﻿using AdoNetMediumConsole.Configuration;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetMediumLib.Configuration
+{
+    public class MainConnector
+    {
+        readonly SqlConnection connection = new SqlConnection(ConnectionString.MsSqlConnection);
+        public async Task<bool> ConnectAsync()
+        {
+
+            bool result;
+            try
+            {
+
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public async void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                await connection.CloseAsync();
+            }
+        }
+        public SqlConnection GetConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                return connection;
+            }
+            else
+            {
+                throw new Exception("Подключение уже закрыто!");
+            }
+        }
+    }
+}
